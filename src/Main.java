74a845cd35af4dee54aec4f2936fdb826da76501
sryabova <ganyim@inbox.ru>
2021-05-29 21:44:09 +0500

    fixed

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         int balance = 100;
         int conditionForBonus = 100;
         int bonus = 0;
-        int payment = 1000;
-        if (payment >= 1000) bonus = payment / conditionForBonus;
+        int payment = 1001;
+        if (payment > 1000) bonus = payment / conditionForBonus;
         balance = balance + payment + bonus;
         System.out.println("Бонус: " + bonus);
         System.out.println("Баланс: " + balance);
