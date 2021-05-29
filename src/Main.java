f29ad4807bc0ca09a90cef0a3e0b323c963ce028
sryabova <ganyim@inbox.ru>
2021-05-29 19:05:43 +0500

    add logic

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int conditionForBonus = 100;
+        int bonus = 0;
+        int payment = 1000;
+        if (payment >= 1000) bonus = payment / conditionForBonus;
+        balance = balance + payment + bonus;
+        System.out.println("Бонус: " + bonus);
+        System.out.println("Баланс: " + balance);
+    }
 }
